17d87fe854c6c1cb012e18540b15ea89bc3afa03
Сергей <scorn17@gmail.com>
2016-06-25 11:43:59 +0300

    Update plugin_code.php

--- plugin/Highslide/plugin_code.php
+++ plugin/Highslide/plugin_code.php
@@ -1,5 +1,11 @@
+//v001
+//26.05.2016
+//Highslide
+//Event: OnWebPagePrerender
+//<a class="highslide" onclick="return hs.expand(this)" href><img class="highslidezoom" /></a>
+//======================================================================================
 $html= $modx->documentOutput;
-if( true )
+if( true || isset( $_GET[ 'test' ] ) )
 {
 	preg_match_all( '/<img(.*)class="(.*)highslidezoom(.*)"(.*)>/imU', $html, $result );
 	
@@ -8,7 +14,7 @@ if( true )
 		foreach( $result[ 0 ] AS $row )
 		{
 			preg_match_all( '/src="(.*)"/imU', $row, $src );
-			$img= $modx->runSnippet( 'ImgCrop6', array( 'img'=>$src[ 1 ][ 0 ], 'wm'=>true ) );
+			$img= $modx->runSnippet( 'ImgCrop45', array( 'img'=>$src[ 1 ][ 0 ], 'wm'=>true ) );
 			$row2= str_replace( $src[ 1 ][ 0 ], $img, $row );
 			if( $src[ 1 ] ) $html= str_replace( $row, '<a class="highslide" onclick="return hs.expand(this)" href="'. $img .'">'. $row2 .'</a>', $html );
 		}

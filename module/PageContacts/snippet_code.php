0ae3645f47a63312b79cb9672cd662eca1ba06de
Сергей <scorn17@gmail.com>
2016-07-04 17:24:22 +0300

    Update snippet_code.php

--- module/PageContacts/snippet_code.php
+++ module/PageContacts/snippet_code.php
@@ -1,7 +1,7 @@
 <?php
-//v03
+//v04
 //PageContacts - snippet
-//17.06.2016
+//01.07.2016
 //=====================================================================================
 $rr= mysql_query( "SELECT * FROM ".$modx->getFullTableName( 'page_contacts' )." ORDER BY block, `index`" );
 if( $rr && mysql_num_rows( $rr ) > 0 )
@@ -17,8 +17,21 @@ if( $rr && mysql_num_rows( $rr ) > 0 )
 			$print .= '<div class="cont_block cont_block_'. $block .' '.( $block%2==0 ? 'cont_block_alt' : '' ).'">'; // открываем новый BLOCK
 		}
 		
-		if( $row[ 'type' ] != 3 )
+		if( $onlyblock && ! $block ) continue;
+		
+		if( $array )
 		{
+			$result_array[ $row['block'] ][]= $row;
+			continue;
+		}
+		
+		if( $row[ 'type' ] == 7 )
+		{
+			$print .= '<div class="cont_img"><a class="highslide" onclick="return hs.expand(this)" href="'. $row[ 'right' ] .'"><img src="'.$modx->runSnippet( 'ImgCrop6', array( 'img'=>$row[ 'right' ], 'w'=>90, 'h'=>90, 'fill'=>true, 'ellipse'=>'max', 'degstep'=>15, 'dopimg'=>'template/images/kruzhok1.png', 'dopimg_xy'=>'0:0' ) ).'" /></a></div>';
+			
+		}elseif( $row[ 'type' ] == 8 ){
+			
+		}elseif( $row[ 'type' ] != 3 ){
 			$print .= '<div class="cont_1">'. $row[ 'left' ] .'</div>';
 			
 			$print .= '<div class="cont_2 '.( $row[ 'br' ] == '1' ? 'cont_br' : '' ).'" '.( $row[ 'type' ] == '4' ? 'style="font-size: 24px;"' : '' ).' '.( $row[ 'type' ] == '6' ? 'style="font-weight:bold;"' : '' ).'>';
@@ -36,5 +49,6 @@ if( $rr && mysql_num_rows( $rr ) > 0 )
 	}
 	$print .= '</div><div class="clr">&nbsp;</div>'; // закрываем последний BLOCK
 }
+if( $array ) return $result_array;
 return $print;
 ?>

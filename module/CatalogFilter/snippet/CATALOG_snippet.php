88eecb1bb06485d4a27bc8b063114eede9e8a257
Сергей <scorn17@gmail.com>
2016-07-22 11:24:31 +0300

    Update CATALOG_snippet.php

--- module/CatalogFilter/snippet/CATALOG_snippet.php
+++ module/CatalogFilter/snippet/CATALOG_snippet.php
@@ -1,20 +1,20 @@
 <?php
 //============================================================================
-$catalog_koren= 17;
+$catalog_koren= 14;
 $catalog_template= 2;
-$MaxItemsInPage= 100;
+$MaxItemsInPage= 4;
 //============================================================================
 
 	
 $myid= $modx->documentIdentifier;
 if( empty( $id ) ) $id= $myid;
 //$doc= $modx->getDocument( $id, 'id,pagetitle,isfolder,content,introtext' );
-$doc= $modx->runSnippet( 'GetDoc6', array( 'ids'=>$id, 'type'=>'this', 'fields'=>'pagetitle,isfolder,content,introtext,parent', 'tvfileds'=>'', 'isf'=>'all' ) );
+$doc= $modx->runSnippet( 'GetDoc6', array( 'ids'=>$id, 'type'=>'this', 'fields'=>'pagetitle,isfolder,content,introtext,parent', 'tvfields'=>'image', 'isf'=>'all' ) );
 $doc= $doc[ $id ];
 
 
-//$lvl= $modx->runSnippet( 'GetLvl', array( 'koren'=>$catalog_koren, 'id'=>$id ) );
-//$maincategory= $modx->runSnippet( 'GetIdOnLvl', array( 'koren'=>$catalog_koren, 'id'=>$id ) );
+$lvl= $modx->runSnippet( 'GetLvl', array( 'koren'=>$catalog_koren, 'id'=>$id ) );
+$maincategory= $modx->runSnippet( 'GetIdOnLvl', array( 'koren'=>$catalog_koren, 'id'=>$id ) );
 
 
 $ParentsPublishedList_flag= $modx->runSnippet( 'ParentsPublishedList', array( 'koren'=>$catalog_koren, 'id'=>$id ) );
@@ -41,7 +41,7 @@ if( $doc[ 'isfolder' ] == 1 )
 	{
 		$docs= array();
 		$docs2= $modx->runSnippet( 'GetDoc6', array( 'ids'=>$id, 'idslist'=>$filter_ids[0], 'type'=>'childs', 'fields'=>'pagetitle,isfolder,introtext,menuindex,parent',
-											   'tvfileds'=>'cat_img,cat_price', 'isf'=>'0', 'sort'=>'menuindex', 'limit'=>$page_s.",".$MaxItemsInPage ) );
+											   'tvfields'=>'image,price,edizmerenia,birka', 'isf'=>'0', 'sort'=>'menuindex', 'limit'=>$page_s.",".$MaxItemsInPage ) );
 		
 		$docs_items_count= $modx->runSnippet( 'GetDoc6', array( 'ids'=>$id, 'idslist'=>$filter_ids[0], 'type'=>'childs', 'isf'=>'0' ) );
 		$docs_items_count= count( $docs_items_count );
@@ -50,7 +50,7 @@ if( $doc[ 'isfolder' ] == 1 )
 		$docs= $modx->runSnippet( 'GetDoc6', array( 'ids'=>$id, 'type'=>'childs', 'depth'=>'1', 'fields'=>'pagetitle,isfolder', 'isf'=>'1', 'sort'=>'menuindex' ) );
 		
 		$docs2= $modx->runSnippet( 'GetDoc6', array( 'ids'=>$id, 'type'=>'childs', 'depth'=>'1', 'fields'=>'pagetitle,isfolder,introtext,menuindex,parent',
-												'tvfileds'=>'cat_img,cat_price', 'isf'=>'0', 'sort'=>'menuindex', 'limit'=>$page_s.",".$MaxItemsInPage ) );
+												'tvfields'=>'image,price,edizmerenia,birka', 'isf'=>'0', 'sort'=>'menuindex', 'limit'=>$page_s.",".$MaxItemsInPage ) );
 		
 		$docs_items_count= $modx->runSnippet( 'GetDoc6', array( 'ids'=>$id, 'type'=>'childs', 'depth'=>'1', 'isf'=>'0' ) );
 		$docs_items_count= count( $docs_items_count );
@@ -67,11 +67,11 @@ if( $doc[ 'isfolder' ] == 1 )
 			if( $row[ 'isfolder' ] == 1 || $itemprinttype == 'category' )
 			{
 				$ii++;
-				$categories .= $modx->runSnippet( 'CAT_ITEM', array( 'type'=>'category', 'row'=>$row, 'last'=>( $ii % ( $indexcatalog || $id == 17 ? 4 : 3 ) == 0 ? true : false ), 'indexcatalog'=>$indexcatalog ) );
+				$categories .= $modx->runSnippet( 'CAT_ITEM', array( 'id'=>$id, 'type'=>'category', 'row'=>$row, 'last'=>( $ii % 3 == 0 ? true : false ), 'indexcatalog'=>$indexcatalog ) );
 				
 			}else{
 				$iii++;
-				$items .= $modx->runSnippet( 'CAT_ITEM', array( 'type'=>'item', 'row'=>$row, 'last'=>( $iii % 4 == 0 ? true : false ) ) );
+				$items .= $modx->runSnippet( 'CAT_ITEM', array( 'type'=>'item', 'row'=>$row, 'last'=>( $iii % 2 == 0 ? true : false ) ) );
 			}
 		}
 	}
@@ -79,12 +79,16 @@ if( $doc[ 'isfolder' ] == 1 )
 	if( ! empty( $doc[ 'content' ] ) ) $content .= $doc[ 'content' ];
 	
 }elseif( true ){
-	$iteminfo= $modx->runSnippet( 'GetDoc6', array( 'ids'=>$id, 'type'=>'this', 'fields'=>'pagetitle,isfolder,content,introtext,parent', 'tvfileds'=>'cat_img,cat_price,cat-opis,har', 'isf'=>'all' ) );
-	$item_page .= $modx->runSnippet( 'CAT_ITEM', array( 'type'=>'itempage', 'row'=>$iteminfo[ $id ] ) );
+	$docinfo= $modx->runSnippet( 'GetDoc6', array( 'ids'=>$id, 'type'=>'this', 'fields'=>'pagetitle,isfolder,introtext,menuindex,parent,content',
+												  'tvfields'=>'image,price,edizmerenia,birka', 'isf'=>'0' ) );
+	$item_page .= $modx->runSnippet( 'CAT_ITEM', array( 'type'=>'itempage', 'thispage'=>true, 'row'=>$docinfo[ $id ] ) );
 }
 
 
 if( $type != 'content' ) $print .= '<div id="ajax_content">';
+
+
+if( $filterparam_flag && ! $filter_ids[0] ) $print .= '<div class="catalog_notfound">Товар не найден!<br />Задайте другие параметры.</div>';
 
 
 $print .= '<div id="catalog" class="catalog '.( $indexcatalog ? 'catalogindex' : 'catalogpage' ).'" itemscope itemtype="http://schema.org/Product">';
@@ -96,12 +100,22 @@ if( ! empty( $categories ) )
 }
 if( ! empty( $items ) )
 {
+	$pages= $modx->runSnippet( 'Pagination', array( 'docs_items_count'=>$docs_items_count, 'myid'=>$id, 'active_page'=>$page, 'MaxItemsInPage'=>$MaxItemsInPage, 'filterpr'=>$filterpr ) );
+	$print .= $pages . ( $pages ? '<br /><br /><br />' : '' );
+}
+if( ! empty( $items ) )
+{
 	$print .= $items;
 	$print .= '<div class="clr">&nbsp;</div>';
 }
 if( ! empty( $item_page ) )
 {
 	$print .= $item_page;
+}
+if( ! empty( $items ) )
+{
+	$print .= '<div class="clr">&nbsp;</div>';
+	$print .= ( $pages ? '<br /><br />' : '' ) . $pages;
 }
 $print .= '</div>';
 

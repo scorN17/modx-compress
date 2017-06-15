9fd76856e7a6149fbb878789e158165cd921b322
Сергей <scorn17@gmail.com>
2017-06-15 14:33:44 +0300

    Update plugin_2_code.php

--- module/CatalogFilter/plugin/plugin_2_code.php
+++ module/CatalogFilter/plugin/plugin_2_code.php
@@ -1,12 +1,17 @@
-//v02
-//Очистка кэша фильтра
-//Event: OnDocFormSave
-//===================================================================
+/**
+ * CF_Clear_Values_Cache
+ *
+ * Очистка кэша фильтра
+ *
+ * @version   10.0
+ * @date      17.05.2017
+ * @events    OnDocFormSave
+ *
+ * @dependence
+ */
+
 $e= &$modx->Event;
-if( $e->name == 'OnDocFormSave' )
-{
-	$myid= $e->params[ 'id' ];
-	$myparent= $modx->getDocument( $myid, 'parent' );
-	$myparent= $myparent[ 'parent' ];
-	mysql_query( "UPDATE ".$modx->getFullTableName( '_cat_filter_values_cache' )." SET dt=0, enabled='n' WHERE catid={$myparent}" );
-}
+$myid= $e->params['id'];
+$myparent= $modx->getDocument($myid, 'parent');
+$myparent= $myparent['parent'];
+if($myparent) $modx->db->query("UPDATE ".$modx->getFullTableName('_catfilter_value_cache')." SET dt=0, e='n' WHERE folderid={$myparent}");

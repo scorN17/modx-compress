acbef883101689df0bbaa3fbb36a4afd407044ae
Сергей <scorn17@gmail.com>
2017-06-15 14:37:05 +0300

    Update AJAX_snippet.php

--- module/CatalogFilter/snippet/AJAX_snippet.php
+++ module/CatalogFilter/snippet/AJAX_snippet.php
@@ -1,8 +1,28 @@
-<?php
-$id= intval( $_GET[ 'id' ] );
-
-if( $_GET[ 'act' ] == 'catalogfilter_get_items' )
+if($_GET['act'] == 'catalogfilter_get_items')
 {
-	return $modx->runSnippet( 'CATALOG', array( 'id'=>$id, 'type'=>'content', 'filterpr'=>$_REQUEST[ 'filterpr' ] ) );
+	return $modx->runSnippet('CATALOG', array('id'=>$id, 'type'=>'content', '_x'=>$_REQUEST['_x']));
 }
-?>
+
+if($_GET['act'] == 'catalogfilter_sort_params')
+{
+	$prms= trim($_GET['prms']);
+	if( ! $prms) return;
+	$prms= explode('/',$prms);
+	if(is_array($prms) && count($prms))
+	{
+		foreach($prms AS $row)
+		{
+			$row= explode(',',$row);
+			$id= intval(array_shift($row));
+			sort($row);
+			
+			$prms_a[$id]= implode('-',$row);
+		}
+		ksort($prms_a);
+		foreach($prms_a AS $key => $row)
+		{
+			$result .= (!empty($result)?'/':'') .$key.'-'.$row;
+		}
+	}
+	return $result;
+}

a97c4042b20a2c84062b00fbdbe3e2467b79032b
Сергей <scorn17@gmail.com>
2017-06-15 14:34:17 +0300

    Update plugin_3_code.php

--- module/CatalogFilter/plugin/plugin_3_code.php
+++ module/CatalogFilter/plugin/plugin_3_code.php
@@ -1,30 +1,38 @@
-//v02
-//Очистка лишних значений фильтра каталога
-//Event: OnBeforeCacheUpdate
-//====================================================================================
-if( ! $_SESSION[ 'CatalogFilterValuesCleared' ] )
+/**
+ * CF_Clear_Excess_Values
+ *
+ * Очистка лишних значений фильтра каталога
+ *
+ * @version   10.0
+ * @date      17.05.2017
+ * @events    OnBeforeCacheUpdate
+ *
+ * @dependence
+ */
+
+if( ! $_SESSION['CatalogFilterValuesCleared'])
 {
-	$_SESSION[ 'CatalogFilterValuesCleared' ]= false;
+	$_SESSION['CatalogFilterValuesCleared']= false;
 	
 	print '<br /><br />';
-	$rr= mysql_query( "SELECT iddoc FROM ".$modx->getFullTableName( '_cat_filter_value' )." GROUP BY iddoc" );
-	if( $rr && mysql_num_rows( $rr ) > 0 )
+	$rr= $modx->db->query("SELECT itemid FROM ".$modx->getFullTableName('_catfilter_value')." GROUP BY itemid");
+	if($rr && $modx->db->getRecordCount($rr))
 	{
-		while( $row= mysql_fetch_assoc( $rr ) )
+		while($row= $modx->db->getRow($rr))
 		{
-			$rr2= mysql_query( "SELECT id FROM ".$modx->getFullTableName( 'site_content' )." WHERE id={$row[iddoc]} LIMIT 1" );
-			if( $rr2 && mysql_num_rows( $rr2 ) == 0 )
+			$rr2= $modx->db->query("SELECT id FROM ".$modx->getFullTableName('site_content')." WHERE id={$row[itemid]} LIMIT 1");
+			if($rr2 && $modx->db->getRecordCount($rr2)==0)
 			{
-				mysql_query( "DELETE FROM ".$modx->getFullTableName( '_cat_filter_value' )." WHERE iddoc={$row[iddoc]}" );
-				print $row[ 'iddoc' ] .'; ';
+				$modx->db->query("DELETE FROM ".$modx->getFullTableName('_catfilter_value')." WHERE itemid={$row[itemid]}");
+				print $row['itemid'].'; ';
 				continue;
 			}
 
-			$rr2= mysql_query( "SELECT id FROM ".$modx->getFullTableName( 'site_content' )." WHERE id={$row[iddoc]} AND isfolder=1 LIMIT 1" );
-			if( $rr2 && mysql_num_rows( $rr2 ) == 1 )
+			$rr2= $modx->db->query("SELECT id FROM ".$modx->getFullTableName('site_content')." WHERE id={$row[itemid]} AND isfolder=1 LIMIT 1");
+			if($rr2 && $modx->db->getRecordCount($rr2))
 			{
-				mysql_query( "DELETE FROM ".$modx->getFullTableName( '_cat_filter_value' )." WHERE iddoc={$row[iddoc]}" );
-				print $row[ 'iddoc' ] .'; ';
+				$modx->db->query("DELETE FROM ".$modx->getFullTableName('_catfilter_value')." WHERE itemid={$row[itemid]}");
+				print $row['itemid'].'; ';
 			}
 		}
 	}

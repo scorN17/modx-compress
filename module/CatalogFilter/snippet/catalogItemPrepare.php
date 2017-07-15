abc29923ee63c8801878c1363de3b309686a3eaf
Сергей <scorn17@gmail.com>
2017-07-15 17:44:06 +0300

    Update catalogItemPrepare.php

--- module/CatalogFilter/snippet/catalogItemPrepare.php
+++ module/CatalogFilter/snippet/catalogItemPrepare.php
@@ -1,10 +1,23 @@
 <?php
 $data['pagetitle']= $modx->runSnippet('Typograph',array('t'=>$data['pagetitle']));
 
-$data['tv.images']= explode('||',$data['tv.images'])[0];
+$images= explode('||',$data['tv.images']);
+
+$data['tv.images']= array_shift($images);
 $data['tv.images_mini']= $data['tv.images'];
 $data['tv.images']= $modx->runSnippet('ImgCrop72',array('img'=>$data['tv.images'], 'w'=>1111, 'h'=>1111));
 $data['tv.images_mini']= $modx->runSnippet('ImgCrop72',array('img'=>$data['tv.images_mini'], 'w'=>222, 'h'=>222, 'fill'=>true));
+
+if(is_array($images) && count($images))
+{
+	foreach($images AS $img)
+	{
+		$i= $modx->runSnippet('ImgCrop72',array('img'=>$img, 'w'=>1111, 'h'=>1111));
+		$mi= $modx->runSnippet('ImgCrop72',array('img'=>$img, 'w'=>100, 'h'=>100, 'fill'=>true));
+		
+		$data['mini_images'] .= '<a data-fancybox="catitempage" href="'.$i.'"><img src="'.$mi.'" /></a>';
+	}
+}
 
 if($data['tv.discount'])
 {
@@ -33,9 +46,9 @@ if(is_array($prms) && count($prms))
 				<div class="cipp_lb">'.$prm['name'].'</div>
 				<div class="cipp_vl">';
 				
-			foreach($prm['values'] AS $vls)
+			foreach($prm['values'] AS $vl)
 			{
-				$data['cf.prms'].= '<div>'.($cc>=2?'&mdash; ':'') .$vls.'</div>';
+				$data['cf.prms'].= '<div>'.($cc>=2?'&mdash; ':'') .$vl.'</div>';
 			}
 			
 			$data['cf.prms'].= '</div><br></div>';

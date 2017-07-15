3e37a063adee7fc3c170db9b2e400aa0e111425a
Сергей <scorn17@gmail.com>
2017-07-15 17:42:32 +0300

    Update CatalogFilter_Props.php

--- module/CatalogFilter/snippet/CatalogFilter_Props.php
+++ module/CatalogFilter/snippet/CatalogFilter_Props.php
@@ -1,10 +1,30 @@
 <?php
+/**
+ * CatalogFilter_Props
+ *
+ * Параметры фильтра
+ *
+ * @version 7.0
+ * @date    15.07.2017
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+	
 $catalogroot= 15; // Корень каталога
 
 //-----------------------------------------------------------------------------------
 
 $parentslist= $modx->runSnippet('GetIdOnLvl', array('id'=>$id, 'koren'=>$catalogroot));
-if(is_array($parentslist) && count($parentslist)) foreach($parentslist AS $row) if($row['id']) $qq .= ( ! empty($qq) ? " OR " : "") ."cf.folders LIKE '%,{$row[id]},%'";
+if(is_array($parentslist) && count($parentslist))
+	foreach($parentslist AS $row)
+		if($row['id'])
+			$qq .= ( ! empty($qq) ? " OR " : "") ."cf.folders LIKE '%,{$row[id]},%'";
 
 if($qq) $rr= $modx->db->query("SELECT *, cf.id AS cfid, cfv.id AS cfvid
 	FROM ".$modx->getFullTableName('_catfilter')." AS cf
@@ -20,11 +40,28 @@ if($rr && $modx->db->getRecordCount($rr))
 		{
 			if(true)
 			{
+				if($row['value'])
+				{
+					$md5= md5($row['value'] .($row['dop']?'-'.$row['dop']:''));
+					$res[$row['cfid'] ]['values'][$md5]= $row['value'] .($row['dop']?' &ndash; '.$row['dop']:'');
+				}
+				
+			}elseif(false){
+				$vals= explode('||', $row['value']);
+				if(is_array($vals) && count($vals))
+					foreach($vals AS $vv)
+						if($vv)
+							$res[$row['cfid'] ]['values'][md5($vv .($row['dop'])?'-'.$row['dop']:'')]= $vv .($row['dop']?' &ndash; '.$row['dop']:'');
+				
+				
+			}elseif(false){
 				$vals= explode('||', $row['value']);
 				if(is_array($vals) && count($vals))
 					foreach($vals AS $vv)
 						if($vv)
 							$res[$row['cfid'] ]['values'][md5($vv)]= $vv;
+				
+				
 				
 			}elseif( false ){
 				$vals= explode( "||", $row[ 'value' ] );

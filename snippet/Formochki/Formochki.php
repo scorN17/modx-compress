f2984508ebcda80e270c44bd8358c4e4fa1b8365
Сергей <scorn17@gmail.com>
2018-01-31 15:48:19 +0300

    Update Formochki.php

--- snippet/Formochki/Formochki.php
+++ snippet/Formochki/Formochki.php
@@ -4,8 +4,8 @@
  * 
  * Формочки
  * 
- * @version     2.0
- * @date        05.01.2018
+ * @version     2.1
+ * @date        31.01.2018
  *
  *
  *
@@ -13,19 +13,20 @@
 	
 //----------------------------------------------------------------------------------
 //КОМУ (через запятую)
-$mailto= 'sergey.it@delta-ltd.ru';
+$mailto       = 'sergey.it@delta-ltd.ru';
 
 //ОТ
-$mailfrom= 'feedback.noreply@yandex.ru';
+$mailfrom     = 'feedback.noreply@yandex.ru';
+$mailfromname = '';
 
 //ОТВЕТИТЬ КОМУ
-$mailreplyto= false;
+$mailreplyto  = false;
 
 //Видимые копии (через запятую)
-$mailcc= false;
+$mailcc       = false;
 
 //Скрытые копии (через запятую)
-$mailbcc= false;
+$mailbcc      = false;
 
 //SNIPPET Formochki
 //----------------------------------------------------------------------------------
@@ -49,150 +50,174 @@ $debug= false;
  *
  */
 
-if ($_GET['act']=='formochki_send') {
-	// CAPTCHA --------------------------------------------------------------------------
-	if (false) {
-		$captcha_flag= false;
-		$reg_captcha= $_POST['g-recaptcha-response'];
-		if ($_SESSION['g-recaptcha'][$reg_captcha]) {
-			$captcha_flag= true;
-		} elseif ($reg_captcha) {
-			$postdata= array(
-				'secret'    => '6LeOGiYUAAAAANUTeFo9eT9MEjKltc0lxq9tQLSa',
-				'response'  => $reg_captcha,
-				'remoteip'  => $_SERVER['REMOTE_ADDR']
-			);
-			$curloptions= array(
-				CURLOPT_URL               => 'https://www.google.com/recaptcha/api/siteverify',
-				CURLOPT_RETURNTRANSFER    => true,
-				CURLOPT_POST              => true,
-				CURLOPT_POSTFIELDS        => $postdata
-			);
-			$curl= curl_init();
-			curl_setopt_array($curl, $curloptions);
-			$curlresult= curl_exec($curl);
-			if ( ! curl_errno($curl)) {
-				$curlresult= json_decode($curlresult, true);
-				if ($curlresult['success'] === true) {
-					$captcha_flag= true;
-					$_SESSION['g-recaptcha'][$reg_captcha]= true;
-				}
-			} else {
-				$result= '{"result":"error","text":"Ошибка. Попробуйте позже или обратитесь к администратору."}';
-			}
-			curl_close($curl);
-		}
-		if ( ! $result && ! $captcha_flag) {
-			$result= '{"result":"error","text":"Вы робот?"}';
+if (isset($_GET['formochki']) && $modx->hasPermission('save_user')) {
+	$limit = isset($_GET['limit']) ? $modx->db->escape($_GET['limit']) : '10';
+	$rr = $modx->db->query("SELECT * FROM ".$modx->getFullTableName('formochki')." ORDER BY id DESC LIMIT ".$limit);
+	if ($rr) {
+		while ($row = $modx->db->getRow($rr)) {
+			print $row;
+			print '<br><hr><br>';
 		}
 	}
-	// CAPTCHA --------------------------------------------------------------------------
-	
-	
-	$frm_privacy_policy= $_POST['frm_privacy_policy']=='y' ? 'y' : 'n';
-	$frm_formid=         intval($_POST['frm_formid']);
-	$frm_name=           trim($_POST['frm_name']);
-	$frm_email=          trim($_POST['frm_email']);
-	$frm_phone=          trim($_POST['frm_phone']);
-	$frm_kogda=          trim($_POST['frm_kogda']);
-	$frm_pageid=         intval($_POST['frm_pageid']);
-	$frm_text=           trim($_POST['frm_text']);
-	$frm_text2=          str_replace("\r", '', $frm_text);
-	$frm_text2=          str_replace("\n", '<br>', $frm_text2);
-	
-	if ( ! $result && $frm_privacy_policy != 'y') {
-		//$result= '{"result":"error","text":"Не отправлено! Необходимо согласиться на обработку персональных данных."}';
-	}
-	
-	if ( ! $result && $frm_formid == 3 && ( ! $frm_name || ! $frm_email || ! $frm_text)) {
-		$result= '{"result":"error","text":"Заполните обязательные поля"}';
-	}
-	
-	if ($result) {
-	} elseif ( ! $frm_email && ! $frm_phone) {
-		$result= '{"result":"error","text":"Укажите контактные данные"}';
-	}
-	
-	if ( ! $result) {
-		$subject= $subjects[$frm_formid];
-		if ( ! $subject) $subject= 'Сообщение';
-		$subject .= ' с сайта '.MODX_SITE_URL;
+}
 
-		$message= '<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title>'.$subject.'</title></head><body><h2>'.$subject.'</h2>';
+if ($_GET['act'] != 'formochki_send') {
+	return;
+}
 
-		if ($frm_pageid) $message .= '<p><b>Отправлено со страницы:</b> '.$modx->makeUrl($frm_pageid,'','','full').'</p>';
+$modx->db->query("CREATE TABLE IF NOT EXISTS ".$modx->getFullTableName('formochki')." (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mail` longtext NOT NULL,
+  `dt` varchar(31) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf-8 AUTO_INCREMENT=1 ;");
 
-		if ($frm_name) $message .= '<p><b>Имя Отчество:</b> '.$frm_name.'</p>';
-
-		if ($frm_email) $message .= '<p><b>E-mail:</b> '.$frm_email.'</p>';
-
-		if ($frm_phone) $message .= '<p><b>Контактный телефон:</b> '.$frm_phone.'</p>';
-
-		if ($frm_kogda) $message .= '<p><b>Когда позвонить?</b> '.$frm_kogda.'</p>';
-
-		$message .= '<p><b>Дата и время сообщения:</b> '.date('d.m.Y, H:i').'</p>';
-
-		if($frm_text2) $message .= '<p><b>Сообщение:</b><br />'.$frm_text2.'</p>';
-
-		$message .= '</body></html>';
-		
-		
-		//------------------------------------------------------------------------------
-		if (true) {
-			$modx->loadExtension('modxmailer');
-			
-			if ($debug) {
-				$modx->mail->SMTPDebug= 2;
-				$modx->mail->Debugoutput = 'html';
+// CAPTCHA --------------------------------------------------------------------------
+if (false) {
+	$captcha_flag= false;
+	$reg_captcha= $_POST['g-recaptcha-response'];
+	if ($_SESSION['g-recaptcha'][$reg_captcha]) {
+		$captcha_flag= true;
+	} elseif ($reg_captcha) {
+		$postdata= array(
+			'secret'    => '6LeOGiYUAAAAANUTeFo9eT9MEjKltc0lxq9tQLSa',
+			'response'  => $reg_captcha,
+			'remoteip'  => $_SERVER['REMOTE_ADDR']
+		);
+		$curloptions= array(
+			CURLOPT_URL               => 'https://www.google.com/recaptcha/api/siteverify',
+			CURLOPT_RETURNTRANSFER    => true,
+			CURLOPT_POST              => true,
+			CURLOPT_POSTFIELDS        => $postdata
+		);
+		$curl= curl_init();
+		curl_setopt_array($curl, $curloptions);
+		$curlresult= curl_exec($curl);
+		if ( ! curl_errno($curl)) {
+			$curlresult= json_decode($curlresult, true);
+			if ($curlresult['success'] === true) {
+				$captcha_flag= true;
+				$_SESSION['g-recaptcha'][$reg_captcha]= true;
 			}
-			
-			$modx->mail->From= $mailfrom;
-			$modx->mail->Sender= $mailfrom;
-			$modx->mail->FromName= '';
-			
-			$modx->mail->isHTML(true);
-			$modx->mail->Subject= $subject;
-			$modx->mail->Body= $message;
-			
-			if ($mailreplyto || $frm_email) {
-				$modx->mail->addReplyTo($mailreplyto ? $mailreplyto : $frm_email);
-			}
-			
+		} else {
+			$result= '{"result":"error","text":"Ошибка. Попробуйте позже или обратитесь к администратору."}';
+		}
+		curl_close($curl);
+	}
+	if ( ! $result && ! $captcha_flag) {
+		$result= '{"result":"error","text":"Вы робот?"}';
+	}
+}
+// CAPTCHA --------------------------------------------------------------------------
+
+
+$frm_privacy_policy = $_POST['frm_privacy_policy'] == 'y' ? 'y' : 'n';
+$frm_formid         = intval($_POST['frm_formid']);
+$frm_name           = trim($_POST['frm_name']);
+$frm_email          = trim($_POST['frm_email']);
+$frm_phone          = trim($_POST['frm_phone']);
+$frm_kogda          = trim($_POST['frm_kogda']);
+$frm_pageid         = intval($_POST['frm_pageid']);
+$frm_text           = trim($_POST['frm_text']);
+$frm_text2          = str_replace("\r", '', $frm_text);
+$frm_text2          = str_replace("\n", '<br>', $frm_text2);
+
+if ( ! $result && $frm_privacy_policy != 'y') {
+	//$result = '{"result":"error","text":"Не отправлено! Необходимо согласиться на обработку персональных данных."}';
+}
+
+if ( ! $result && $frm_formid == 3 && ( ! $frm_name || ! $frm_email || ! $frm_text)) {
+	$result = '{"result":"error","text":"Заполните обязательные поля"}';
+}
+
+if ($result) {
+} elseif ( ! $frm_email && ! $frm_phone) {
+	$result = '{"result":"error","text":"Укажите контактные данные"}';
+}
+
+if ( ! $result) {
+	$subject = $subjects[$frm_formid];
+	if ( ! $subject) $subject = 'Сообщение';
+	$subject .= ' с сайта '.MODX_SITE_URL;
+
+	$message = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title>'.$subject.'</title></head><body><h2>'.$subject.'</h2>';
+
+	if ($frm_pageid) $message .= '<p><b>Отправлено со страницы:</b> '.$modx->makeUrl($frm_pageid,'','','full').'</p>';
+
+	if ($frm_name) $message .= '<p><b>Имя Отчество:</b> '.$frm_name.'</p>';
+
+	if ($frm_email) $message .= '<p><b>E-mail:</b> '.$frm_email.'</p>';
+
+	if ($frm_phone) $message .= '<p><b>Контактный телефон:</b> '.$frm_phone.'</p>';
+
+	if ($frm_kogda) $message .= '<p><b>Когда позвонить?</b> '.$frm_kogda.'</p>';
+
+	$message .= '<p><b>Дата и время сообщения:</b> '.date('d.m.Y, H:i').'</p>';
+
+	if($frm_text2) $message .= '<p><b>Сообщение:</b><br />'.$frm_text2.'</p>';
+
+	$message .= '</body></html>';
+	
+	$message_db = $modx->db->escape($message);
+	$modx->db->query("INSERT INTO ".$modx->getFullTableName('formochki')." SET mail='{$message}', dt='".date('d.m.Y, H:i')."'");
+
+	//------------------------------------------------------------------------------
+	if (true) {
+		$modx->loadExtension('modxmailer');
+
+		if ($debug) {
+			$modx->mail->SMTPDebug   = 2;
+			$modx->mail->Debugoutput = 'html';
+		}
+
+		$modx->mail->From     = $mailfrom;
+		$modx->mail->Sender   = $mailfrom;
+		$modx->mail->FromName = $mailfromname;
+
+		$modx->mail->isHTML(true);
+		$modx->mail->Subject = $subject;
+		$modx->mail->Body    = $message;
+
+		if ($mailreplyto || $frm_email) {
+			$modx->mail->addReplyTo($mailreplyto ? $mailreplyto : $frm_email);
+		}
+
+		if ($mailto) {
 			$mailto= explode(',', $mailto);
 			foreach ($mailto AS $row) {
 				$modx->mail->addAddress(trim($row));
 			}
-			
-			if ($mailcc) {
-				$mailcc= explode(',', $mailcc);
-				foreach ($mailcc AS $row) {
-					$modx->mail->addCC(trim($row));
-				}
-			}
-			
-			if ($mailbcc) {
-				$mailbcc= explode(',', $mailbcc);
-				foreach ($mailbcc AS $row) {
-					$modx->mail->addBCC(trim($row));
-				}
-			}
-			
-			$phpmailer_result= $modx->mail->Send();
 		}
-		//------------------------------------------------------------------------------
-		
-		
-		if ($phpmailer_result) {
-			$result= '{"result":"ok","text":"Сообщение отправлено!"}';
-		} else {
-			$result= '{"result":"error","text":"Ошибка! Повторите попытку позже."}';
+
+		if ($mailcc) {
+			$mailcc= explode(',', $mailcc);
+			foreach ($mailcc AS $row) {
+				$modx->mail->addCC(trim($row));
+			}
 		}
+
+		if ($mailbcc) {
+			$mailbcc= explode(',', $mailbcc);
+			foreach ($mailbcc AS $row) {
+				$modx->mail->addBCC(trim($row));
+			}
+		}
+
+		$phpmailer_result = $modx->mail->Send();
 	}
-	
-	print $result;
-	
-	if (isset($_GET['ajax'])) {
-		header('Content-Type:text/html; charset=UTF-8');
-		exit();
+	//------------------------------------------------------------------------------
+
+
+	if ($phpmailer_result) {
+		$result = '{"result":"ok","text":"Сообщение отправлено!"}';
+	} else {
+		$result = '{"result":"error","text":"Ошибка! Повторите попытку позже."}';
 	}
+}
+
+print $result;
+
+if (isset($_GET['ajax'])) {
+	header('Content-Type:text/html; charset=UTF-8');
+	exit();
 }

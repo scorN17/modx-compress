e90e9c33e14f1e2ca0331443898c73196635a188
Сергей <scorn17@gmail.com>
2018-03-07 12:43:31 +0300

    Update formochki.js

--- snippet/Formochki/formochki.js
+++ snippet/Formochki/formochki.js
@@ -2,36 +2,60 @@
 (function($){
 
 	$(document).ready(function(){
+		$('.formochki .frm_itm').each(function(){
+			var elem = $(this);
+			var input = $('input[type="text"]',this);
+			input.focus(function(){
+				elem.addClass('focustext');
+			});
+			input.focusout(function(){
+				if (input.val() == '') {
+					elem.removeClass('focustext');
+				}
+			});
+		});
 	});
 
 	$(window).on('load',function(){
-		$('.formochki .frm_submit').each(function(){
-			if( ! $(this).hasClass('frm_submit_load'))
-			{
-				$(this).addClass('frm_submit_load');
-				var elem= $(this).parent();
-				while( ! elem.hasClass('formochki')) elem= elem.parent();
+		$('.formochki .frm_submit button').each(function(){
+			var elem= $(this).parent();
+			while( ! elem.hasClass('formochki'))
+				elem= elem.parent();
 
-				$('.frm_result',elem).hide();
+			$('.frm_result',elem).hide();
 
-				$('form',elem).submit(function(){ return false; });
+			$('form',elem).submit(function(){
+				return false;
+			});
 
-				$(this).click(function(){
-					$('form .setdefaultvalue input',elem).each(function(){
-						if($(this).val()==$(this).data('default')) $(this).val('');
-					});
-					$.post($('form',elem).attr('action')+"?ajax&act=formochki_send", $('form',elem).serialize()).done(function(data){
-							var result= $.parseJSON(data);
-							$('.frm_result',elem).show().removeClass('frm_result_error').removeClass('frm_result_ok').addClass('frm_result_'+result.result).html(result.text);
-							if(result.result=='ok') $('form',elem).remove(); else{
-								$('form .setdefaultvalue input',elem).each(function(){
-									if($(this).val()=='') $(this).val($(this).data('default'));
-								});
-							}
-						});
-					return;
+			$(this).click(function(){
+				if(elem.hasClass('frm_submit_load')) return;
+				elem.addClass('frm_submit_load');
+				var formData= new FormData($('form',elem)[0]);
+				var ajaxresult = $.ajax({
+					url: $('form',elem).attr('action')+'?ajax&act=formochki_send',
+					data: formData,
+					processData: false,
+					contentType: false,
+					type: 'POST',
+					dataType: 'JSON',
+					cache: false
 				});
-			}
+				ajaxresult.done(function(result){
+					elem.removeClass('frm_submit_load');
+					if (result.result == 'ok') {
+						$('form',elem).remove();
+						$('.frm_result_ok',elem)
+							.html(result.text)
+							.show();
+					} else {
+						$('.frm_result_error',elem)
+							.html(result.text)
+							.show();
+					}
+				});
+				return;
+			});
 		});
 	});
 })(jQuery);
